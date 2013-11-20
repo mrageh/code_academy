a4e0fac5f70776700db099e3e154afb21c765410
Adam89 <Adamm1@aston.ac.uk>
2013-11-19 21:47:56 -0700

    just finished basic typo

--- adventure_game.js
+++ adventure_game.js
@@ -23,7 +23,7 @@ if(ready === 'yes'){
       console.log("This is just the beginning of my game empire. Stay tuned for more!");
   }
   else{
-      console.log("I slaved away at this game and you gave me that score?! The nerve! Just you wait!");
+      console.log("I slaved away at this game and you gave me that score?! The nerve! Just you wait!.");
   }
 }
 else{
